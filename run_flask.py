18bd0bc16bc89019d17712a5a1e8ebc1e52f015e
durgar2012 <durga@vt.edu>
2018-12-14 11:36:03 +0000

    uploading the front-end files

--- run_flask.py
+++ run_flask.py
@@ -1,86 +1,124 @@
 import pandas as pd
 from flask import Flask , render_template, request
 from collections import Counter
+from movie_clusters import *
+import random
 
 app = Flask(__name__)
 
-mov_1 = ""
-mov_2 = ""
-likedOptionsName = []
-likedOptionsCluster = []
-
-df = pd.read_csv("CSV/clustered_Data.csv")
+mov_1 = "_"
+mov_2 = "_"
+return_liked_movies = []
+new_list_count = 1
 
 
+movie_list, all_data_clustered = get_data()
+print("This is the list of movie options:::")
+print(movie_list)
 #####################  SUPPORTING FUNCTIONS  #####################
 
 def chooseNewTitles():
-	randomRow = df.sample(n=1)
-	opt_1_name = randomRow['original_title'].item()
-	opt_1_custer = randomRow['Cluster'].item()
+	global movie_list
+	global new_list_count
+	global return_liked_movies
 
-	opt_2_name = df[df.Cluster != opt_1_custer].sample(n=1)['original_title'].item()
+	#check if list is empty
+	if movie_list:
+		#if not empty
+		randomRow = random.choice(movie_list)
+	else:
+
+		movie_list = process_response_iterate(np.array(return_liked_movies))
+		return_liked_movies = []
+		randomRow = random.choice(movie_list)
+
+		new_list_count = new_list_count + 1
+
+	opt_1_name = randomRow[0]
+	opt_1_custer = randomRow[1]
+
+	movie_list.remove(randomRow)
+
+	opt_2 = random.choice(movie_list)
+	opt_2_name = opt_2[0]
+
+	movie_list.remove(opt_2)
 
 	return opt_1_name, opt_2_name
 
 #DETERMINE LOGIC...for right now I'm going to select the mode cluster number
 def chooseTitleOnDecisions():
 	modeClusterNum = Counter(data).most_common(1)[0][0]
-	
+	return modeClusterNum
 
 
 def getClusterNum(title):
-	df.loc[df['original_title'] == title, 'Cluster'].iloc[0]
+	return all_data_clustered.loc[all_data_clustered['original_title'] == title, 'clusters'].iloc[0]
 
 def getTitleName(cluster):
-	df.loc[df['Cluster'] == cluster, 'original_title'].iloc[0]
+	return all_data_clustered.loc[all_data_clustered['clusters'] == cluster, 'original_title'].iloc[0]
 
+def format_rec(recommendation_list):
+	clean_list = []
+	for rec in recommendation_list:
+		movie_name = rec[0]
+		clean_list.append(movie_name)
+	return ' and '.join(clean_list)
 
 #####################  FLASK METHODS  #####################
 @app.route('/')
 def homePage():
-	return render_template("movie_temp.html")
+	return render_template("movie_temp.html", your_rec = "Nothing yet! Get a recommendation!")
 
 
-@app.route('/recommendations/')
+@app.route('/recommendations/', methods=['POST', 'GET'])
 def recommendationsPage():
+	global mov_1
+	global mov_2
+	global return_liked_movies
 
-	"""
-	Go to the Recommendation page
-	Show two random movie trailers - user will click on the option that they prefer
-	Send the selected option to python so that it can append to the likedOptions list
-	Get the cluster # of the last likedOption and get another movie from the same cluster and compare it to a movie from a different cluster
-    """
+	num_rounds = 2
+   
+	if request.method == 'POST':
+		if 'opt_1' in request.form or 'opt_2' in request.form:
+			if 'opt_1' in request.form:
+				print("movie 1=" + mov_1)
 
-	if 'submit_button' in request.form:
-		if request.form['submit_button'] == 'Option 1':
-			likedOptionsName.append(mov_1)
-			likedOptionsCluster.append(getClusterNum(mov_1))
+				return_liked_movies.append([mov_1,getClusterNum(mov_1)])
+				if new_list_count < num_rounds:
+					mov_1, mov_2  =  chooseNewTitles() #opt_1
+				else:
+					print("recommendation_fin!!!")
+					recommendation_fin = get_knn(np.array(return_liked_movies), 2)
+					print(recommendation_fin)
+					rtrn_rec = format_rec(recommendation_fin)
+					return render_template("movie_temp.html", your_rec = rtrn_rec)
 
-		elif request.form['submit_button'] == 'Option 2':
-			likedOptionsName.append(mov_2)
-			likedOptionsCluster.append(getClusterNum(mov_1))
+			elif 'opt_2' in request.form:
+				print("movie 2=" + mov_2)
 
-		#define new mov_1 and mov_2
+				return_liked_movies.append([mov_2,getClusterNum(mov_2)])
+				if new_list_count < num_rounds:
+					mov_1, mov_2  =  chooseNewTitles() #opt_2
+				else:
+					print("get recommendation_fin!!!")
+					recommendation_fin = get_knn(np.array(return_liked_movies), 2, all_data_clustered)
+					print(recommendation_fin)
+					rtrn_rec = format_rec(recommendation_fin)
+					return render_template("movie_temp.html", your_rec = rtrn_rec)
 
-
-
-	#this is the first time the user is selecting an option
+			else:
+				print("neither... but this is impossible")
 	else:
-		if mov_1 == "" or mov_2 == "":
-			#if either movie one or movie 2 do not have values then we will randomly assign movie titles to these variables
-			mov_1, mov_2 = chooseNewTitles()
+		if new_list_count < num_rounds:
+			mov_1, mov_2 = chooseNewTitles() #else
 	
 	youtube_url_1 = "https://www.youtube.com/embed?listType=search;list=" + mov_1
 	youtube_url_2 = "https://www.youtube.com/embed?listType=search;list=" + mov_2
 
-	return render_template("recommendations.html", mov_name_1 = youtube_url_1, mov_name_2 = youtube_url_2)	
+	return render_template("recommendations.html", mov_name_1 = youtube_url_1, mov_name_2 = youtube_url_2, name_1 = mov_1, name_2 = mov_2)	
 
 
 if __name__=='__main__':
     app.run(debug = True)
 
-df['treated'] = 0
-new_df = df[df['agent_id']==i]
-if new_df['got_software'].astype(str).str.contains('1').any():
-	df.loc[df['agent_id'] == i, 'treated'] = 1
