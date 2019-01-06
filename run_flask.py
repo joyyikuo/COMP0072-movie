ea12782dc71fb6b5028bcd823ed3809581779799
durgar2012 <durga@vt.edu>
2019-01-06 12:39:46 +0000

    Update run_flask.py
    updated some logic and fixed the issue that occurred during the demo

--- run_flask.py
+++ run_flask.py
@@ -11,10 +11,10 @@ mov_2 = "_"
 return_liked_movies = []
 new_list_count = 1
 
+all_data_clustered = pd.read_csv("all_data_clustered.csv")
 
-movie_list, all_data_clustered = get_data()
-print("This is the list of movie options:::")
-print(movie_list)
+movie_list = get_data()
+
 #####################  SUPPORTING FUNCTIONS  #####################
 
 def chooseNewTitles():
@@ -28,7 +28,7 @@ def chooseNewTitles():
 		randomRow = random.choice(movie_list)
 	else:
 
-		movie_list = process_response_iterate(np.array(return_liked_movies))
+		movie_list = process_response_iterate(all_data_clustered, np.array(return_liked_movies))
 		return_liked_movies = []
 		randomRow = random.choice(movie_list)
 
@@ -46,7 +46,6 @@ def chooseNewTitles():
 
 	return opt_1_name, opt_2_name
 
-#DETERMINE LOGIC...for right now I'm going to select the mode cluster number
 def chooseTitleOnDecisions():
 	modeClusterNum = Counter(data).most_common(1)[0][0]
 	return modeClusterNum
@@ -61,7 +60,7 @@ def getTitleName(cluster):
 def format_rec(recommendation_list):
 	clean_list = []
 	for rec in recommendation_list:
-		movie_name = rec[0]
+		movie_name = "'" + str(rec[0]) + "'"
 		clean_list.append(movie_name)
 	return ' and '.join(clean_list)
 
@@ -70,52 +69,82 @@ def format_rec(recommendation_list):
 def homePage():
 	return render_template("movie_temp.html", your_rec = "Nothing yet! Get a recommendation!")
 
-
+'''
+The purpose of this method is to grab the newest user selected movies and then propose new movies to recommend to them.
+Once they have selected about 2 - 3 rounds of preferences the app will redirect them to the home page and propose movie suggestions
+'''
 @app.route('/recommendations/', methods=['POST', 'GET'])
 def recommendationsPage():
+	#global variables
 	global mov_1
 	global mov_2
 	global return_liked_movies
+	global new_list_count
 
 	num_rounds = 2
    
 	if request.method == 'POST':
+		#If the reommendation form has previously been initiated determine which round of recommendations the user is one
 		if 'opt_1' in request.form or 'opt_2' in request.form:
 			if 'opt_1' in request.form:
-				print("movie 1=" + mov_1)
 
-				return_liked_movies.append([mov_1,getClusterNum(mov_1)])
 				if new_list_count < num_rounds:
+					#Append to the selected movie list
+					return_liked_movies.append([mov_1,getClusterNum(mov_1)])
 					mov_1, mov_2  =  chooseNewTitles() #opt_1
-				else:
-					print("recommendation_fin!!!")
+
+				elif new_list_count == num_rounds:
+					new_list_count = new_list_count + 1
+					return_liked_movies.append([mov_1,getClusterNum(mov_1)])
 					recommendation_fin = get_knn(np.array(return_liked_movies), 2)
-					print(recommendation_fin)
 					rtrn_rec = format_rec(recommendation_fin)
+
+					#renders the home page - with the proposed recommendations
 					return render_template("movie_temp.html", your_rec = rtrn_rec)
+
+
+				#if the user has already selected 3 movies and wanted to continue to get another recommendation, 
+				#we will ask them two go throw 2 more rounds of movie selections
+				else:
+					new_list_count = 1
+					return_liked_movies.clear()
+					return_liked_movies.append([mov_1,getClusterNum(mov_1)])
+					mov_1, mov_2  =  chooseNewTitles() #opt_1
 
 			elif 'opt_2' in request.form:
-				print("movie 2=" + mov_2)
 
-				return_liked_movies.append([mov_2,getClusterNum(mov_2)])
 				if new_list_count < num_rounds:
+					#Append to the selected movie list					
+					return_liked_movies.append([mov_2,getClusterNum(mov_2)])
 					mov_1, mov_2  =  chooseNewTitles() #opt_2
-				else:
-					print("get recommendation_fin!!!")
-					recommendation_fin = get_knn(np.array(return_liked_movies), 2, all_data_clustered)
-					print(recommendation_fin)
-					rtrn_rec = format_rec(recommendation_fin)
-					return render_template("movie_temp.html", your_rec = rtrn_rec)
 
-			else:
-				print("neither... but this is impossible")
+				elif new_list_count == num_rounds:
+					new_list_count = new_list_count + 1
+					return_liked_movies.append([mov_2,getClusterNum(mov_2)])
+					recommendation_fin = get_knn(np.array(return_liked_movies), 2)
+					rtrn_rec = format_rec(recommendation_fin)
+
+					#renders the home page - with the proposed recommendations
+					return render_template("movie_temp.html", your_rec = rtrn_rec)
+				
+				#if the user has already selected 3 movies and wanted to continue to get another recommendation, 
+				#we will ask them two go throw 2 more rounds of movie selections
+				else:
+					new_list_count = 1
+					return_liked_movies.clear()
+					return_liked_movies.append([mov_2,getClusterNum(mov_2)])
+					mov_1, mov_2  =  chooseNewTitles() #opt_2
+
+	#Else propose 2 new movies for the user to choose between
 	else:
 		if new_list_count < num_rounds:
 			mov_1, mov_2 = chooseNewTitles() #else
-	
+
+	#Pull the youtube trailers for the movies the user will be chosing between	
 	youtube_url_1 = "https://www.youtube.com/embed?listType=search;list=" + mov_1
 	youtube_url_2 = "https://www.youtube.com/embed?listType=search;list=" + mov_2
 
+	#Render the recommendation html page
 	return render_template("recommendations.html", mov_name_1 = youtube_url_1, mov_name_2 = youtube_url_2, name_1 = mov_1, name_2 = mov_2)	
 
 
